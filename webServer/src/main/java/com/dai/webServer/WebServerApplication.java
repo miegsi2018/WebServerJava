4c7bf294b3856334681e609ff29d65cd9cb2e128
Tiagoacb <33163740+Tiagoacb@users.noreply.github.com>
2018-04-21 10:07:51 +0100

    atualização de pakages

--- webServer/src/main/java/com/dai/webServer/WebServerApplication.java
+++ webServer/src/main/java/com/dai/webServer/WebServerApplication.java
@@ -3,6 +3,8 @@ package com.dai.webServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dai.webServer.mqtt.Listener;
+
 @SpringBootApplication
 public class WebServerApplication extends Listener {
 
