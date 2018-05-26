6f32bd7b1fc19822c096c808dd1e663803aab9c0
Tiagoacb <33163740+Tiagoacb@users.noreply.github.com>
2018-05-26 11:36:30 +0100

    rfid

--- webServer/src/main/java/com/dai/test/Main.java
+++ webServer/src/main/java/com/dai/test/Main.java
@@ -8,8 +8,8 @@ public class Main extends Listener{
 	
 	public static void main(String[] args) {
 	       
-		Listener listener = new Listener();
-	        listener.subscribe("data/fds");
+		//Listener listener = new Listener();
+	      //  listener.subscribe("data/fds");
 	
 		ReceiveRequests requests = new ReceiveRequests();
 	        requests.subscribe("requestString");
