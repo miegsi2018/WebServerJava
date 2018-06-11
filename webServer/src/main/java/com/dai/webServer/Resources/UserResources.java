b94ea503885c2d55eca1c1b05645c7015c087a0e
vitormiegsi <vhoa1995@hotmail.com>
2018-06-11 16:47:16 +0100

    fd

--- webServer/src/main/java/com/dai/webServer/Resources/UserResources.java
+++ webServer/src/main/java/com/dai/webServer/Resources/UserResources.java
@@ -38,9 +38,6 @@ public class UserResources {
 	public List<User> retrieveAllUsers() {
 		return utilizadorRepository.findAll();
 	}
-	
-	/* Metodo get invocado pelo web site onde o mesmo envia o id do utilizador 
-	 * e o java devolve todos os dados do utilizador com esse id */
 
 	@GetMapping("/utilizador/{id}")
 	public User retrieveUser(@PathVariable long id) {
@@ -51,11 +48,7 @@ public class UserResources {
 
 		return utilizador.get();
 	}
-	
-	/* Metodo post invocado pelo web site onde o mesmo envia o o email e password do utilizador 
-	 * e o java verifica se o email e pass correspondem a user valido e envia para o 
-	 * web site a respota ao pedido de login */
-	
+	//IDK tho
 	@PostMapping("/login")
 	public User verifyUser(@RequestBody User utilizador) {
 		
@@ -78,8 +71,6 @@ public class UserResources {
 	
 	}
 
-	/* Metodo post invocado pelo web site onde o mesmo envia o email do utilizador 
-	 * e o java devolve todos os dados do utilizador associados a esse email */
 	
 	@PostMapping("/mail")
 
@@ -94,30 +85,27 @@ public class UserResources {
 	
 	
 	}	
+
 	
-	public User findUserByEmail(String email) {
-			
-			Optional<User> utilizador = utilizadorRepository.findByEmail(email);
 	
-			if (!utilizador.isPresent())
-				throw new UtilizadorNotFoundException("id-" + email);
 	
-			return utilizador.get();
+public User findUserByEmail(String email) {
 		
-	}
+		Optional<User> utilizador = utilizadorRepository.findByEmail(email);
+
+		if (!utilizador.isPresent())
+			throw new UtilizadorNotFoundException("id-" + email);
+
+		return utilizador.get();
 	
-	
-	/* Metodo delete invocado pelo web site onde o mesmo envia o id do utilizador 
-	 * e o java apaga o utilizador associados a esse id */
-	
+
+
+}
 	@DeleteMapping("/utilizador/{id}")
 	public void deleteUser(@PathVariable long id) {
 		utilizadorRepository.deleteById(id);
 	}
 
-	/* Metodo post invocado pelo web site onde o mesmo envia todos os daddos do utilizador 
-	 * e o java adicionar o utilizador associados a bd */
-	
 	@PostMapping("/utilizador")
 	public ResponseEntity<Object> createUser(@RequestBody User utilizador) {
 		
@@ -126,6 +114,11 @@ public class UserResources {
 
 		Optional<User> exist = utilizadorRepository.findByEmail(mailReturned);
 		if (exist.isPresent()){
+
+		
+
+
+
 
 		System.out.println("not");
 
@@ -140,16 +133,14 @@ public class UserResources {
 				.buildAndExpand(savedUser.getId_user()).toUri();
 		
 		return ResponseEntity.created(location).build();
+		
 	
 	}
 	
 
-	/* Metodo post invocado pelo web site onde o mesmo envia a cor da luz pretendida 
-	 * e o java faz parser a mensagem recebida invoca o metodo sendMessage enviado para ele
-	 * o topic, rgb e id e altera a luz via mqtt */
-	
+
 	@PostMapping("/changeLight")
-	public void createUser(@RequestBody String light) throws ParseException {
+	public String createUser(@RequestBody String light) throws ParseException {
 
 		
 		JSONParser parser = new JSONParser();
@@ -168,12 +159,12 @@ public class UserResources {
 		String correctedTopic = "request/"  + topic;
 		pub.sendMessage(correctedTopic , message ,"lightChange");	
 
+		
+		return done;
+
 	}
 
 
-	/* Metodo put invocado pelo web site onde o mesmo envia os dados do utilizador que serão
-	 * alterados e o java faz a alteração na bd*/
-	
 	@PutMapping("/utilizador/{id}")
 	public ResponseEntity<Object> updateUser(@RequestBody User utilizador, @PathVariable long id) {
 
